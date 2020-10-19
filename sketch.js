ffa8623bdcbf4a42afb82c17be6046dde3561ca9
Raviju <rjrohtak@gmail.com>
2020-10-19 16:51:49 +0545

    declared all veriables

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ const World= Matter.World;
 const Bodies = Matter.Bodies;
 
 var engine, world;
-var box1, pig1;
+var box1, pig1,box2,box3,box4,box5,pig3,log1,log2,log3,log4,log5;
 var backgroundImg,platform;
 
 function preload() {
